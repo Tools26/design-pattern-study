c8e67db43a4c90e60b7c087a2b22d879ca30ef05
김영균 <kyoungyun98@naver.com>
2022-11-07 19:25:33 +0900

    Decorator pattern with swift

---
+++ 3_decorator_pattern/younggyun/Example2.swift
@@ -0,0 +1,105 @@
+protocol Rice {
+    var name: String { get set }
+    func cost() -> Int
+    func getName() -> String
+}
+
+extension Rice {
+    func getName() -> String {
+        return name
+    }
+}
+
+class BarleyRice: Rice { // 보리밥
+    var name = "보리밥"
+    func cost() -> Int {
+        return 1500
+    }
+}
+
+class BrownRice: Rice { // 현미밥
+    var name = "현미밥"
+    func cost() -> Int {
+        return 1000
+    }
+}
+
+protocol IngredientDecorator: Rice {
+    var rice: Rice { get set }
+}
+
+class Spam: IngredientDecorator {
+    var name = "스팸"
+    var rice: Rice
+
+    init(_ r: Rice) {
+        self.rice = r
+    } 
+
+    func cost() -> Int {
+        return 1000 + rice.cost()
+    } 
+
+    func getName() -> String {
+        return self.name + rice.getName()
+    }
+}
+
+class Tuna: IngredientDecorator {
+    var name = "참치"
+    var rice: Rice
+    
+    init(_ r: Rice) {
+        self.rice = r
+    } 
+
+    func cost() -> Int {
+        return 700 + rice.cost()
+    } 
+
+    func getName() -> String {
+        return self.name + rice.getName()
+    }
+}
+
+class Chicken: IngredientDecorator {
+    var name = "치킨"
+    var rice: Rice
+    
+    init(_ r: Rice) {
+        self.rice = r
+    } 
+
+    func cost() -> Int {
+        return 900 + rice.cost()
+    } 
+
+    func getName() -> String {
+        return self.name + rice.getName()
+    }
+}
+
+class Bulgogi: IngredientDecorator {
+    var name = "불고기"
+    var rice: Rice
+    
+    init(_ r: Rice) {
+        self.rice = r
+    } 
+
+    func cost() -> Int {
+        return 1200 + rice.cost()
+    } 
+
+    func getName() -> String {
+        return self.name + rice.getName()
+    }
+}
+
+var mayo = Bulgogi(Tuna(Spam(BarleyRice())))
+print("메뉴: ", mayo.getName() + "마요")
+print("가격: ", mayo.cost())
+
+var mayo2 = Chicken(Spam(BrownRice()))
+print("메뉴: ", mayo2.getName() + "마요")
+print("가격: ", mayo2.cost())
