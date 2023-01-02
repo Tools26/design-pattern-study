bd9fa16516eb1f05c0694e247b55ea3bf6fa969d
Justice <63052097+juijeong8324@users.noreply.github.com>
2023-01-02 21:23:58 +0900

    9_template method 의정

---
+++ 9_template_method_pattern/UiJeong/Anchor.java
@@ -0,0 +1,6 @@
+public class Anchor extends ModelRenderable{ // 일반 앵커인 경우
+    @Override
+    protected void thenAccept() {
+        System.out.println("해당 위치에 앵커 노드 생성");
+    }
+}
---
+++ 9_template_method_pattern/UiJeong/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Anchor anchor = new Anchor();
+        CloudAnchor cloudAnchor = new CloudAnchor();
+
+        anchor.createModel(1);
+        cloudAnchor.createModel(2);
+    }
+}
