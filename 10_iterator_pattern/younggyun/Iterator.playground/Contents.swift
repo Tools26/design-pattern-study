6af14a9045abbc230839f8afbd133d62656b140f
김영균 <kyoungyun98@naver.com>
2023-01-15 00:02:44 +0900

    iterator pattern

--- 10_iterator_pattern/younggyun/Iterator.playground/Contents.swift
+++ 10_iterator_pattern/younggyun/Iterator.playground/Contents.swift
@@ -5,4 +5,19 @@ map.add(element: "일번", for: "1번")
 map.add(element: "이번", for: "2번")
 map.add(element: "삼번", for: "3번")
 
-var iterator = map.makeIterator()
+var iterator: any Iterator = map.makeIterator()
+while iterator.hasNext(),
+      let elem = iterator.next() {
+  print(elem)
+}
+
+var list = CustomList<Int>()
+list.append(3)
+list.append(4)
+list.append(2)
+
+iterator = list.makeIterator()
+while iterator.hasNext(),
+      let elem = iterator.next() {
+  print(elem)
+}
