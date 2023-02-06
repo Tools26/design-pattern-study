e5bfb46e3936feb435ad366059f63c2fcf4e587a
Justice <63052097+juijeong8324@users.noreply.github.com>
2023-02-06 14:31:04 +0900

    Delete Main.java

--- 13_proxy_pattern/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String args[]) {
-        Download download = new Proxy_DownloadFile("CS.pdf");
-        Download download2 = new Proxy_DownloadFile("test.txt");
-
-        download.downloadFile();
-        download2.downloadFile();
-    }
-}
